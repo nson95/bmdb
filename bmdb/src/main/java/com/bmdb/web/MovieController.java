7e0a5a0274629ea5ea955eda450af2abfe475870
nson95 <nicksonnenberg95@gmail.com>
2019-02-26 13:47:34 -0500

    add method for movie controller added

--- bmdb/src/main/java/com/bmdb/web/MovieController.java
+++ bmdb/src/main/java/com/bmdb/web/MovieController.java
@@ -3,6 +3,7 @@ package com.bmdb.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,6 +24,15 @@ public class MovieController {
 		}
 		catch (Exception e) {
 			return JsonResponse.getErrorInstance("Error getting all movies.", e);
+		}
+	}
+	
+	@GetMapping(path="/Get/{id}")
+	public @ResponseBody JsonResponse getMovie(@PathVariable int id) {
+		try {
+			return JsonResponse.getInstance(movieRepo.findById(id));
+		} catch (Exception e) {
+			return JsonResponse.getErrorInstance("Error getting movie", e);
 		}
 	}
 }
