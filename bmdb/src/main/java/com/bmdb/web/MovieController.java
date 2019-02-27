772c5e8bbe5722fef438ec5922ec9d7d32c9e75f
nson95 <nicksonnenberg95@gmail.com>
2019-02-27 12:42:16 -0500

    save/change/add mapping added to movie controller

--- bmdb/src/main/java/com/bmdb/web/MovieController.java
+++ bmdb/src/main/java/com/bmdb/web/MovieController.java
@@ -1,12 +1,16 @@
 package com.bmdb.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.bmdb.business.Movie;
 import com.bmdb.business.MovieRepository;
 import com.bmdb.util.JsonResponse;
 
@@ -35,4 +39,23 @@ public class MovieController {
 			return JsonResponse.getErrorInstance("Error getting movie", e);
 		}
 	}
+	private @ResponseBody JsonResponse saveMovie(@RequestBody Movie movie) {
+		try {
+			movieRepo.save(movie);
+			return JsonResponse.getInstance(movie);
+		} catch (DataIntegrityViolationException ex) {
+			return JsonResponse.getErrorInstance(ex.getRootCause().toString(), ex);
+		} catch (Exception ex) {
+			return JsonResponse.getErrorInstance(ex.getMessage(), ex);
+		}
+	}
+	@PostMapping(path="/Add")
+	public @ResponseBody JsonResponse addMovie(Movie movie) {
+		return saveMovie(movie);
+	}
+	@PostMapping(path="/Change")
+	public @ResponseBody JsonResponse changeMovie(Movie movie) {
+		return saveMovie(movie);
+	}
+	
 }
